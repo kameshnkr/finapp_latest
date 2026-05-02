42b6d2994995176605a4b8cd7eba0f14a92fe4f8
Kamesh <kamesh.nkr@gmail.com>
2026-05-02 13:42:36 +0530

    - increased the balance adjustment sheet height

--- frontend_flutter/lib/ui/home/account_balance_adjust_sheet.dart
+++ frontend_flutter/lib/ui/home/account_balance_adjust_sheet.dart
@@ -280,7 +280,7 @@ class _AccountBalanceAdjustSheetState
 
     return DraggableScrollableSheet(
       expand: false,
-      initialChildSize: 0.85,
+      initialChildSize: 0.93,
       minChildSize: 0.5,
       maxChildSize: 1.0,
       builder: (context, scrollCtrl) {
